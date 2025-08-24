27cd45ab5e8e652b1c377c5f45bbf4311359834b
milazao <milena.peracetta.silva@escola.pr.gov.br>
2025-08-24 18:22:56 -0300

    ata

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,55 @@
+// Superlative
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Superlative',
+    'What is the highest mountain in the world?',
+    'Mount Everest is the highest mountain in the world.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Superlative',
+    'What is the longest river in Africa?',
+    'The Nile is the longest river in Africa.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Superlative',
+    'Which is the coldest continent in the world?',
+    'Antarctica is the coldest continent in the world.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Superlative',
+    'Which is the most populous capital in Europe?',
+    'London is the most populous capital in Europe.'
+)
+
+// Comparative
+criaCartao(
+    'Comparative',
+    'Is the Nile longer than the Amazon River?',
+    'Yes, the Nile is longer than the Amazon River.'
+)
+
+criaCartao(
+    'Comparative',
+    'Is Greenland colder than Canada?',
+    'Yes, Greenland is colder than most parts of Canada.'
+)
+
+criaCartao(
+    'Comparative',
+    'Is the Sahara Desert bigger than the Gobi Desert?',
+    'Yes, the Sahara Desert is bigger than the Gobi Desert.'
+)
+
+criaCartao(
+    'Comparative',
+    'Is Tokyo more populous than New York City?',
+    'Yes, Tokyo is more populous than New York City.'
+)
+
+criaCartao(
+    'Comparative',
+    'Is La Paz higher than Quito?',
+    'Yes, La Paz is higher than Quito.'
 )
